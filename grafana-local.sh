39031452065af3c1699fd622ad866220bd6793da
Naman-UbU <namansondhiya17@gmail.com>
2025-09-10 15:05:54 +0530

    resolve any errors

--- grafana-local.sh
+++ grafana-local.sh
@@ -1,21 +1,18 @@
 sudo apt update
 
+
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
+sudo wget -q -O /usr/share/keyrings/grafana.key
 
-sudo mkdir -p /etc/apt/keyrings/
-wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
-
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
 # update 
 sudo apt-get update
 # install 
 sudo apt-get install grafana
-
 sudo /bin/systemctl status grafana-server
 sudo /bin/systemctl start grafana-server
-
 # sudo /bin/systemctl enable grafana-server.service
 
 echo "Installed Succesfully"
