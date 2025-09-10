a22fdd00f287b9c6f525251f4ab394e4b1b50de7
Naman-UbU <namansondhiya17@gmail.com>
2025-09-10 15:08:01 +0530

    resolved errors :v2

--- grafana-local.sh
+++ grafana-local.sh
@@ -1,9 +1,8 @@
 sudo apt update
 
-
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
-sudo wget -q -O /usr/share/keyrings/grafana.key
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
 
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
